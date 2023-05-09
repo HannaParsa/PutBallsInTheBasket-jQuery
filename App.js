f962e34e3f5b0266b1cac8f51da00f976c7d6cfd
hanna-parsa-80 <hanaparsa_80@yahoo.com>
2023-05-09 08:52:15 +0330

    processing

--- App.js
+++ App.js
@@ -2,5 +2,13 @@ $(function(){
     var counter = 0;
     $("button").On("click", function(){
         $("#container").appendTo("<span class='ball-in'></span>");
+        var arr=[ 'red', 'blue', 'yellow', 'lightgrey', 'darkorchid', 'black', 'orange', 'deeppink', 'green', 'purple', 'saddlebrown', 'lightseagreen', 'deepskyblue', 'firebrick', 'crimson']
+        const colNum=Math.floor((Math.random() * arr.length) + 0);
+        var spans=$("span");
+        const sp=spans.eq(count);
+        count=count+1;
+        sp.css({
+            "background-color":ballColor
+        });
     })
 })
